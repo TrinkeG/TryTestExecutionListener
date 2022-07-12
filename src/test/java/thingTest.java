2409a94505f43378c842a06032d8db7418f2e248
Christina Ripley-Gonzalez <christina.ripleyg@gmail.com>
2022-07-12 15:01:08 +0100

    initial commit

---
+++ src/test/java/thingTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class thingTest {
+
+  @Test
+  @RMMID()
+  public void do_a_thing(){
+    System.out.println("RMMTags: 1234,5647,8790 ");
+    assertTrue(true);
+  }
+}
